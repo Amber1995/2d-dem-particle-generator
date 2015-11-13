4be3c4c6e07c54aa67043ebed27b6834aa291ae6
Yishu Wang <yw407@cam.ac.uk>
2015-11-13 17:50:20 +0000

    removed merge conflict

--- generate_particles.cpp
+++ generate_particles.cpp
@@ -2,12 +2,6 @@
 #include <ctime>
 #include <cstdlib>
 
-<<<<<<< HEAD
 int main() {
   std::cout << "DEM particle generator" << std::endl;
-=======
-///!!!generate particles!!!///
-int main() {
-  
->>>>>>> 699a4f13b7c67e9133800d1cef661c1d312b9892
 }
