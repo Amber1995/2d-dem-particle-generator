42df72949e74342c6e2e83a31ac7f9f8a56201c0
Yishu Wang <yw407@cam.ac.uk>
2015-11-16 10:05:21 +0000

    create a particle class

--- generate_particles.cpp
+++ generate_particles.cpp
@@ -2,6 +2,21 @@
 #include <ctime>
 #include <cstdlib>
 
-int main() {
-  std::cout << "DEM particle generator" << std::endl;
+class Particle{
+
+public:
+
+  double x,y,z;
+  doule radius;
+  Generate_Paritle()=Particle(x,y,z,radius);
+  
 }
+
+  Generate_Particle::Particle(double x1,double y1,double z1,double r)
+  {
+    x=x1;
+    y=y1;
+    z=z1;
+    radius=r;
+  }
+
